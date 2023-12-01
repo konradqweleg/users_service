66df82ef0149f63a5fd5ec159b2bb74e7c6509db
Konrad <konradnrog@gmail.com>
2023-12-01 22:27:17 +0100

    Code refactored

--- src/test/java/com/example/usersservices_mychatserver/integration/integration/request_util/RequestUtil.java
+++ src/test/java/com/example/usersservices_mychatserver/integration/integration/request_util/RequestUtil.java
@@ -5,32 +5,35 @@ import java.net.URISyntaxException;
 
 public class RequestUtil {
     private int serverPort;
+    private static String prefixHttp = "http://localhost:";
+    private static String prefixServicesApiV1 = "/userServices/api/v1/user";
+
     public RequestUtil(int serverPort) {
         this.serverPort = serverPort;
     }
 
     public URI createRequestLogin() throws URISyntaxException {
-        return new URI("http://localhost:" + serverPort + "/userServices/api/v1/user/login");
+        return new URI(prefixHttp + serverPort + prefixServicesApiV1 + "/login");
     }
 
     public URI createRequestRegister() throws URISyntaxException {
-        return new URI("http://localhost:" + serverPort + "/userServices/api/v1/user/register");
+        return new URI(prefixHttp + serverPort + prefixServicesApiV1 + "/register");
     }
 
     public URI createRequestActiveUserAccount() throws URISyntaxException {
-        return new URI("http://localhost:" + serverPort + "/userServices/api/v1/user/activeUserAccount");
+        return new URI(prefixHttp + serverPort + prefixServicesApiV1 + "/activeUserAccount");
     }
 
     public URI createRequestResendActiveUserAccountCode() throws URISyntaxException {
-        return new URI("http://localhost:" + serverPort + "/userServices/api/v1/user/resendActiveUserAccountCode");
+        return new URI(prefixHttp + serverPort + prefixServicesApiV1 + "/resendActiveUserAccountCode");
     }
 
     public URI createRequestSendResetPasswordCode() throws URISyntaxException {
-        return new URI("http://localhost:" + serverPort + "/userServices/api/v1/user/sendResetPasswordCode");
+        return new URI(prefixHttp + serverPort + prefixServicesApiV1 + "/sendResetPasswordCode");
     }
 
     public URI createRequestResetPassword() throws URISyntaxException {
-        return new URI("http://localhost:" + serverPort + "/userServices/api/v1/user/resetPassword");
+        return new URI(prefixHttp + serverPort + prefixServicesApiV1 + "/resetPassword");
     }
 
 }
